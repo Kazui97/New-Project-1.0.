7967fd538161a093c6e882d5d12c2c612e578370
Kazui97 <camyescobar12@gmail.com>
2019-10-08 15:44:51 -0500

    Initial commit

---
+++ Assets/Juego.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juego : MonoBehaviour
+{
+    public int varx;
+    public int vary;
+    public GameObject buey;
+    public GameObject palomo;
+    public GameObject mono;
+    public GameObject dragon;
+    public GameObject [,]gameObjects;
+
+
+    void Start()
+    {
+        gameObjects = new GameObject[varx , vary];
+        for (int x = 0; x < varx; x++)
+        {
+            for (int y = 0; y < vary; y++)
+            {
+                GameObject buuey = GameObject.Instantiate(buey) as GameObject;
+                GameObject paloma = GameObject.Instantiate(palomo) as GameObject;
+                Vector3 position = new Vector3(x,y,0);
+                buuey.transform.position = position;
+                paloma.transform.position = position;
+                gameObjects[x,y]=buuey;
+                gameObjects[x,y]=paloma;
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
