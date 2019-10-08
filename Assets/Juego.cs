edbf92650111db55c29778358ee3d4835fcb703d
Kazui97 <camyescobar12@gmail.com>
2019-10-08 16:00:23 -0500

    inicio

--- Assets/Juego.cs
+++ Assets/Juego.cs
@@ -15,20 +15,30 @@ public class Juego : MonoBehaviour
 
     void Start()
     {
-        gameObjects = new GameObject[varx , vary];
+        /*gameObjects = new GameObject[varx , vary];
         for (int x = 0; x < varx; x++)
         {
             for (int y = 0; y < vary; y++)
             {
                 GameObject buuey = GameObject.Instantiate(buey) as GameObject;
-                GameObject paloma = GameObject.Instantiate(palomo) as GameObject;
                 Vector3 position = new Vector3(x,y,0);
                 buuey.transform.position = position;
-                paloma.transform.position = position;
                 gameObjects[x,y]=buuey;
-                gameObjects[x,y]=paloma;
+              
             }
+        }*/
+
+        int cambio = Random.Range(0,5)
+        switch (switch_on)
+        {
+            
+            default:
         }
+
+
+
+
+
     }
 
     
